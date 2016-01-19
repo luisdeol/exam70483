27b55eb3dbe67e4b1ee8db5e53b5c8340554ef62
luisfelipekai <luisfelipekai@gmail.com>
2016-01-19 17:51:20 +0100

    Working with Data #1
    DirectoryInfo, FileInfo, Streams

---
+++ C9_Working_with_Data/DirInfo_FileInfo/DirInfo_FileInfo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DirInfo_FileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir_Info = new DirectoryInfo(@"C:\");
+            Console.WriteLine("Files: ");
+            foreach (FileInfo fileInfo in dir_Info.GetFiles())
+            {
+                Console.WriteLine(fileInfo.Name);
+            }
+            Console.WriteLine("Directories: ");
+            foreach(DirectoryInfo directoryInfo in dir_Info.GetDirectories())
+            {
+                Console.WriteLine(directoryInfo.Name);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ C9_Working_with_Data/Streams/Streams/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define path
+            var path = @"C:\Users\luisdeolpy\Documents\Visual Studio 2015\Projects\Exam 70-483\Streams\Streams\Numbers.txt";
+            //Open StreamReader using "using", so the File action thread will be finished once the job is finished.
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                Console.WriteLine("Char by Char");
+                while (!streamReader.EndOfStream)
+                {
+                    Console.WriteLine((char)streamReader.Read());
+                }
+            }
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                Console.WriteLine("Line by Line");
+                while(!streamReader.EndOfStream){
+                    Console.WriteLine(streamReader.ReadLine());
+                }
+            }
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                Console.WriteLine("Entire text");
+                Console.WriteLine(streamReader.ReadToEnd());
+            }
+            Console.Read();
+        }
+    }
+}
