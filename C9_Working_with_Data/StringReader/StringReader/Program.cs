901707b818a8534d65b5d1a36fb57be94e6985ce
luisfelipekai <luisfelipekai@gmail.com>
2016-01-19 18:04:44 +0100

    Working with Data #2

---
+++ C9_Working_with_Data/StringReader/StringReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StringReader_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StringReader stringReader = new StringReader("Hello\nGoodbye"))
+            {
+                int pos = stringReader.Read();
+                while (pos != -1)
+                {
+                    Console.WriteLine("{0}", (char)pos);
+                    pos = stringReader.Read();
+                }
+            }
+            Console.Read();   
+        }
+    }
+}
