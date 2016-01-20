cd4c911e572a0b62b76daa5636fe9e89cad612b7
luisfelipekai <luisfelipekai@gmail.com>
2016-01-20 22:47:15 +0100

    ADO.NET ExecuteReader

---
+++ C9_Working_with_Data/ExecuteReader/ExecuteReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ExecuteReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = "Server=LUISDEOL\\SQLEXPRESS; Database=Infragistics; Integrated Security=SSPI;";
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "SELECT * FROM dbo.City";
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Console.WriteLine(string.Format("Name: {0} , Country: {1}", dr["Name"], dr["Country"]));
+                    }
+                }
+                dr.Close();
+            }
+            Console.Read();
+        }
+    }
+}
