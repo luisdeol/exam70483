35cdf719c67f9f9f19baf1e579a91c50dfa33d67
luisfelipekai <luisfelipekai@gmail.com>
2016-02-04 17:48:02 +0100

    Query Expressions

---
+++ C10_Working_With_LINQ/QueryExpressions/QueryExpressions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*LINQ query expressions enable you to perform “queries” against the array with syntax 
+            similar to SQL except it is C# syntax and the order of the elements is different. 
+            The benefit of a query expression is less coding and more readability.*/
+            int[] myArray = new int[10] { 1,2,3,4,5,6,7,8,9,10};
+            var evenNumbers = from i in myArray
+                              where i % 2 == 0
+                              select i;
+            foreach (int i in evenNumbers)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
