2b5ce753b5e7a0b180445d2d6a42860c2e06a1ca
luisfelipekai <luisfelipekai@gmail.com>
2016-02-21 19:19:18 +0100

    Query Expressions Ordering and Filtering

---
+++ C10_Working_With_LINQ/QueryExpressions_Etc/QueryExpressions_Etc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryExpressions_Etc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Filtering, you can add multiple where clauses in the Query Expression
+            int[] myArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var evenNumbers = from i in myArray
+                              where i % 2 == 0
+                              where i > 5
+                              select i;
+            foreach (int i in evenNumbers)
+                Console.WriteLine(i.ToString());
+            //Ordering
+            var evenNumbers2 = from i in myArray
+                               where i % 2 == 0
+                               orderby i descending
+                               select i;
+        }
+    }
+}
