9f65876c584f7f2bf7a0942ad980706152e53a4c
luisfelipekai <luisfelipekai@gmail.com>
2016-02-24 23:44:17 +0100

    Method-Based and Query Expression PROJECTION

---
+++ C10_Working_With_LINQ/LINQ_And_QueryExpression_Projection/LINQ_And_QueryExpression_Projection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_And_QueryExpression_Projection
+{
+    class Program
+    {
+        class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string State { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>()
+            {
+                new Person() { FirstName = "Luis", LastName = "Oliveira", State = "RN" },
+                new Person() { FirstName = "John", LastName = "Smith", State = "Madrid" }
+            };
+            /*Using Query Expressions! */
+            var lastNameQR = from p in persons
+                             select p.LastName;
+            /* Using Method-based expression*/
+            var lastNameMethodBased = persons.Select(p => p.LastName);
+
+            Console.WriteLine("Using Query Expression: ");
+            foreach (var lastName in lastNameQR)
+            {
+                Console.WriteLine(String.Format("Last Name: {0}", lastName));
+            }
+            Console.WriteLine("Using Method-Based Expression");
+            foreach (var lastName in lastNameMethodBased)
+            {
+                Console.WriteLine(String.Format("Last Name: {0}", lastName));
+            }
+        }
+    }
+}
