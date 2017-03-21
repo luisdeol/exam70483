91e549ecb4354265c4c93461d5c691839bacdbf7
luisfelipekai <luisfelipekai@gmail.com>
2017-03-21 19:29:56 -0300

    System.Reflection: Retrieving Information from Assemblies.

---
+++ C8_Creating_and_Using_Types_with_Reflection_Custom Attributes_CodeDOM_and_Lambda_Expressions/assemblyInfo/assemblyInfo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace assemblyInfo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /* To get access to the PublicKeyToken of an Assembly you need to execute sn.exe and pass the Assembly path.
+           For example, I used this command to retrieve the PublicKeyToken from System.Data assembly
+           "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\sn.exe" -T "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.2\System.Data.dll"
+           */
+            var assembly =
+                Assembly.Load("System.Data, version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
+            var referencedAssemblies = assembly.GetReferencedAssemblies();
+
+            foreach (var reference in referencedAssemblies)
+            {
+                Console.WriteLine($"Assembly Name {reference.FullName}");
+                Console.WriteLine($"Assembly Version {reference.Version}");
+            }
+        }
+    }
+}
