d788aae746d3d650ed7dc26349ee0aee5a864426
luisfelipekai <luisfelipekai@gmail.com>
2017-03-21 17:18:38 -0300

    System.Reflection: Create and Read Custom Attributes.

---
+++ C8_Creating_and_Using_Types_with_Reflection_Custom Attributes_etc/readcreate-customattr/readcreate-customattr/MyCustomAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace readcreate_customattr
+{
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Struct)]
+    public class MyCustomAttribute : Attribute
+    {
+        public enum MyCustomAttributeEnum
+        {
+                Red, 
+                White, 
+                Blue
+        }
+
+        public bool RandomBool { get; set; }
+        public string RandomString { get; set; }
+        public MyCustomAttributeEnum CustomAttributeEnumItem { get; set; }
+    }
+}
---
+++ C8_Creating_and_Using_Types_with_Reflection_Custom Attributes_etc/readcreate-customattr/readcreate-customattr/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace readcreate_customattr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classType = typeof(TestClass);
+            var attribute = (MyCustomAttribute) classType.GetCustomAttribute(typeof(MyCustomAttribute), false);
+
+            Console.WriteLine($"MyCustomAttributeEnum: {attribute.CustomAttributeEnumItem}");
+            Console.WriteLine($"RandomBool: {attribute.RandomBool}");
+            Console.WriteLine($"RandomString: {attribute.RandomString}");
+        }
+    }
+
+    [MyCustom(CustomAttributeEnumItem = MyCustomAttribute.MyCustomAttributeEnum.White, RandomBool = true, RandomString = "hehe")]
+    class TestClass
+    {
+
+    }
+}
