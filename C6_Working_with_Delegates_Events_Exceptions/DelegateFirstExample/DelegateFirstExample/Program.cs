4ff72614e689c774dded7bebc0517ad67c7dae2d
luisfelipekai <luisfelipekai@gmail.com>
2017-06-30 22:11:15 -0300

    Exemplo 1 Delegate

---
+++ C6_Working_with_Delegates_Events_Exceptions/DelegateFirstExample/DelegateFirstExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateFirstExample
+{
+    class Program
+    {
+        private static FunctionDelegate delegateFuncao;
+        static void Main(string[] args)
+        {
+            delegateFuncao = Funcao;
+            Console.WriteLine(delegateFuncao(2.9));
+        }
+        private delegate double FunctionDelegate(double x);
+
+        private static double Funcao(double x)
+        {
+            return (double)(12 * Math.Sin(3 * x) / (1 + Math.Abs(x)));
+        }
+    }
+}
